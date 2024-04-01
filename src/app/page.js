a6f3c5563198626cc99621f452d9cb88a1261670
yhwit30 <yhwit30@gmail.com>
2024-04-01 16:47:49 +0900

    mui, tailwind 충돌방지

--- src/app/page.js
+++ src/app/page.js
@@ -1,48 +1,14 @@
-import * as React from "react";
-import Radio from "@mui/material/Radio";
-import RadioGroup from "@mui/material/RadioGroup";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import FormControl from "@mui/material/FormControl";
-import FormLabel from "@mui/material/FormLabel";
-import Stack from "@mui/material/Stack";
-import Button from "@mui/material/Button";
+'use client';
+import Button from '@mui/material/Button';
 
-export default function RowRadioButtonsGroup() {
+export default function Home() {
   return (
     <>
-      <div className="w-96">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt
-        reiciendis, repudiandae harum soluta, voluptatibus eligendi sapiente
-        sunt dolores, qui rem a consectetur asperiores. Excepturi ipsam nisi
-        dolorum ex, iure quae?
-      </div>
-
-      <Stack spacing={2} direction="row">
-        <Button variant="text">Text</Button>
-        <Button variant="contained">Contained</Button>
-        <Button variant="outlined">Outlined</Button>
-      </Stack>
-
-      <div>so be it! No hubo Remedio 어쩔 도리가 없어</div>
-
-      <FormControl className="p-10">
-        <FormLabel id="demo-row-radio-buttons-group-label">Gender</FormLabel>
-        <RadioGroup
-          row
-          aria-labelledby="demo-row-radio-buttons-group-label"
-          name="row-radio-buttons-group"
-        >
-          <FormControlLabel value="female" control={<Radio />} label="Female" />
-          <FormControlLabel value="male" control={<Radio />} label="Male" />
-          <FormControlLabel value="other" control={<Radio />} label="Other" />
-          <FormControlLabel
-            value="disabled"
-            disabled
-            control={<Radio />}
-            label="other"
-          />
-        </RadioGroup>
-      </FormControl>
+      <div>hihi</div>
+      <div>한글이 잘 보입니까 랜더링 개빠르네</div>
+      <Button className="tw-bg-red-400" variant="contained">
+        button!!
+      </Button>
     </>
   );
 }
