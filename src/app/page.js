fede3339ff0bfeb42e2f18fe38984046eb93d321
yhwit30 <yhwit30@gmail.com>
2024-04-02 15:15:30 +0900

    drawer 적용

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,23 @@
 
 import * as React from 'react';
 import { ThemeProvider } from '@mui/material/styles';
-import { Button, Box, AppBar, Toolbar, Snackbar, Alert as MuiAlert } from '@mui/material';
+import {
+  Button,
+  Box,
+  AppBar,
+  Toolbar,
+  Snackbar,
+  Alert as MuiAlert,
+  Backdrop,
+  CircularProgress,
+} from '@mui/material';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
 import theme from './theme';
 import { FaBars } from 'react-icons/fa';
 
@@ -12,8 +28,37 @@ const Alert = React.forwardRef((props, ref) => {
 
 export default function App() {
   const [open, setOpen] = React.useState(false);
+  const [drawerOpen, setDrawerOpen] = React.useState(false);
 
   const alertRef = React.useRef(null);
+
+  const toggleDrawer = (drawerOpen) => () => {
+    setDrawerOpen(drawerOpen);
+  };
+
+  const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
 
   return (
     <>
@@ -21,7 +66,7 @@ export default function App() {
         <AppBar position="fixed">
           <Toolbar>
             <div className="tw-flex-1">
-              <FaBars className="tw-cursor-pointer" />
+              <FaBars onClick={toggleDrawer(true)} className="tw-cursor-pointer" />
             </div>
             <div className="logo-box">
               <a href="/" className="tw-font-bold">
@@ -38,6 +83,22 @@ export default function App() {
           section
         </section>
       </ThemeProvider>
+
+      <div>
+        <Button onClick={toggleDrawer(true)}>Open drawer</Button>
+        <Drawer open={drawerOpen} onClose={toggleDrawer(false)}>
+          {DrawerList}
+        </Drawer>
+      </div>
+
+      <Button onClick={() => setOpen(true)}>Show backdrop</Button>
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={open}
+        onClick={() => setOpen(false)}>
+        <CircularProgress color="inherit" />
+      </Backdrop>
+
       <section>
         <Button onClick={() => setOpen(true)}>Open Snackbar</Button>
         <Alert ref={alertRef} severity="error" varient="filled">
