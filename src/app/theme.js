6f3da977f632eebdcc715cf4f2d3a183d4188f85
yhwit30 <yhwit30@gmail.com>
2024-04-03 13:37:40 +0900

    Chip 상단 여백, Box sx, theme light,dark

--- src/app/theme.js
+++ src/app/theme.js
@@ -8,9 +8,12 @@ const theme = createTheme({
     type: 'dark',
     primary: {
       main: '#2196f3',
+      light: 'rgb(77, 171, 245)',
+      dark: 'rgb(23, 105, 170)',
+      contrastText: '#fff',
     },
     secondary: {
-      main: '#2196f3',
+      main: '#f50057',
     },
   },
 });
